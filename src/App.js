80975209fe02b371c19ba63f3f9c7312b1e7a680
milad ghasemi <miladghasemi0011@gmail.com>
2023-02-04 22:52:54 +0330

    e

--- src/App.js
+++ src/App.js
@@ -1,45 +1,55 @@
-import { useDispatch } from 'react-redux';
-import { Routes, Route } from 'react-router-dom';
-import {getGenreList} from './features/tmdb/tmdb'
-import axios from 'axios';
-import Main from './layouts/main';
-import Home from './pages/home/home';
-import Movie from './pages/movie/movie';
-import Serie from './pages/serie/serie';
-import Search from './pages/search/search'
-import InGrid from './routes/ingrid/ingrid';
-import DetailPage from './pages/detail-page/detail-page';
-import DetailPerson from './pages/detail-person/detail-person';
+import { useDispatch } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+import { getGenreList } from "./features/tmdb/tmdb";
+import axios from "axios";
+import Main from "./layouts/main";
+import Home from "./pages/home/home";
+import Movie from "./pages/movie/movie";
+import Serie from "./pages/serie/serie";
+import Search from "./pages/search/search";
+import InGrid from "./routes/ingrid/ingrid";
+import DetailPage from "./pages/detail-page/detail-page";
+import DetailPerson from "./pages/detail-person/detail-person";
 
-import NotFound from './pages/notfound/notfound';
-import { Container } from './App.style'
-import { useLocation} from 'react-router-dom'
+import NotFound from "./pages/notfound/notfound";
+import { Container } from "./App.style";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
-  const dispatch = useDispatch()
-  dispatch(getGenreList())
-  const {pathname} = useLocation()
+  const dispatch = useDispatch();
+  dispatch(getGenreList());
+  const { pathname } = useLocation();
+  // const [reRender, setRerender] = useState(0);
 
-  axios.get('https://ipgeolocation.abstractapi.com/v1/?api_key=2812dc4a710c4ec8bf74fb65a8fe0c25')
-  .then(res => {
-    if(res.data.country_code === "IR") return alert(`لطفا با فیلتر شکن وارد شوید
-      حتما لینک وبسایت را دوباره باز کنید`)
-  })
+  axios
+    .get(
+      "https://ipgeolocation.abstractapi.com/v1/?api_key=2812dc4a710c4ec8bf74fb65a8fe0c25"
+    )
+    .then((res) => {
+      // setRerender((i) => i + 1);
+      if (res.data.country_code === "IR")
+        return alert(`لطفا با فیلتر شکن وارد شوید
+      حتما لینک وبسایت را دوباره باز کنید`);
+    });
   return (
-        <Container>
-          <Routes>
-            <Route path='/' element={<Main/>}>
-              <Route index element={<Home/>}/>
-              <Route path='/movie' element={<Movie/>}/>
-              <Route path='/tv' element={<Serie/>}/>
-              <Route path='search' element={<Search/>}/>
-              <Route path='/grid-template/*' element={<InGrid/>}/>
-              <Route path='/detail/:category/:id' element={ <DetailPage key={pathname} />} />
-              <Route path='/person/:id' element={<DetailPerson key={pathname}/>} />
-              <Route path="*" element={<NotFound/>}/>
-            </Route>
-          </Routes>
-        </Container>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route index element={<Home />} />
+          <Route path="/movie" element={<Movie />} />
+          <Route path="/tv" element={<Serie />} />
+          <Route path="search" element={<Search />} />
+          <Route path="/grid-template/*" element={<InGrid />} />
+          <Route
+            path="/detail/:category/:id"
+            element={<DetailPage key={pathname} />}
+          />
+          <Route path="/person/:id" element={<DetailPerson key={pathname} />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Container>
   );
 }
 export default App;
